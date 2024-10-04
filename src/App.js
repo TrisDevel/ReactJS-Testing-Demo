78278bc94c395c0e3a0831d61373b45dff4ecc0d
Trong Tri Bui <buitrongtri2004@gmail.com>
2024-10-04 07:19:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [text, setText] = useState("Information CI/CD Testing");
   return (
     <div>
       <Router>
